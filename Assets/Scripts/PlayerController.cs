ec6aa0152ed5999e43e0d86fb66190cc254477c0
y2kat <lentacomputadora@gmail.com>
2024-03-19 20:14:00 -0600

    Player Movement and Input Actions

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerInputActions inputActions;
+    private float forceMovement = 5;
+
+    private void Awake()
+    {
+        inputActions = new PlayerInputActions();
+        inputActions.Enable();
+    }
+
+    void Start()
+    {
+        inputActions.Standard.Movement.performed += Move;
+    }
+
+    private void Move(InputAction.CallbackContext context)
+    {
+        Vector2 movementInput = context.ReadValue<Vector2>();
+        Vector3 direction = new Vector3(movementInput.x, 0, movementInput.y) * forceMovement;
+        transform.position += direction;
+    }
+}
