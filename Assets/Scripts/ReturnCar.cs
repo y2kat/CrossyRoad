1ea649e2d8d38cd0766a30f1e87a332d2aba7dd8
y2kat <lentacomputadora@gmail.com>
2024-03-21 00:54:15 -0600

    ReturnCar Added

---
+++ Assets/Scripts/ReturnCar.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnCar : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("car"))
+        {
+            other.transform.Translate(0,0,20);
+        }
+    }
+}
